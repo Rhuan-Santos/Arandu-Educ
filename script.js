02b76b1c8119b76cf6dd16ab50d49ee04865391e
Rhuan-Santos <rhuanmatheusdossantos600@gmail.com>
2026-05-09 01:58:58 +0000

    Adiciona pagina inicial do Arandu Educ

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("titulo").textContent =
+"Explore o Mundo, compreenda o passado e descubra como a história molda territórios.";
+
+document.getElementById("quem_somos").textContent =
+"Arandu Educ é um espaço de aprendizado que conecta História e Geografia para ajudar você a entender o mundo de forma completa. Nosso objetivo é formar alunos que não apenas memorizam, mas compreendem quem são e onde estão no mundo.";
+
+document.getElementById("marielly").textContent =
+"Eu sou Marielly Apuara, professora de História e Geografia. Ensino História para que o passado não seja esquecido ou distorcido. ";
